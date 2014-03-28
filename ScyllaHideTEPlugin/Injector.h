b145d1d5be83d1e0f0dbc97d2663f24f0f63c23e
cypherpunk <the.cypher@gmail.com>
2014-03-28 19:13:46 +0100

    initial commit of the TE plugin

---
+++ ScyllaHideTEPlugin/Injector.h
@@ -0,0 +1,9 @@
+#include <windows.h>
+#include "..\HookLibrary\HookMain.h"
+#include "..\InjectorCLI\DynamicMapping.h"
+
+void startInjectionProcess(HANDLE hProcess, BYTE * dllMemory);
+void startInjection(DWORD targetPid, const WCHAR * dllPath);
+BYTE * ReadFileToMemory(const WCHAR * targetFilePath);
+void FillExchangeStruct(HANDLE hProcess, HOOK_DLL_EXCHANGE * data);
+DWORD SetDebugPrivileges();
---
+++ ScyllaHideTEPlugin/ScyllaHideTEPlugin.cpp
@@ -0,0 +1,84 @@
+#include <windows.h>
+#include "TitanEngine.h"
+#include "Injector.h"
+
+BOOL APIENTRY DllMain(HINSTANCE hi, DWORD reason, LPVOID)
+{
+    switch(reason)
+    {
+    case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls(hi);
+        break;
+    }
+    return TRUE;
+}
+
+static void ScyllaHide(DWORD ProcessId) {
+    WCHAR * dllPath = 0;
+
+#ifdef _WIN64
+    dllPath = L"Z:\\__dev\\scyllahide\\Debug\\HookLibrary.dll";
+#else
+    dllPath = L"Z:\\__dev\\scyllahide\\Debug\\HookLibrary.dll";
+#endif
+
+    SetDebugPrivileges();
+    startInjection(ProcessId, dllPath);
+}
+
+extern "C" __declspec(dllexport) void TitanDebuggingCallBack(LPDEBUG_EVENT debugEvent, int CallReason)
+{
+    static DWORD ProcessId;
+    static HANDLE hProcess;
+    switch(CallReason)
+    {
+    case UE_PLUGIN_CALL_REASON_EXCEPTION:
+    {
+        switch(debugEvent->dwDebugEventCode)
+        {
+        case CREATE_PROCESS_DEBUG_EVENT:
+        {
+            hProcess=debugEvent->u.CreateProcessInfo.hProcess;
+            ProcessId=debugEvent->dwProcessId;
+            ScyllaHide(ProcessId);
+        }
+        break;
+
+        case EXCEPTION_DEBUG_EVENT:
+        {
+            switch(debugEvent->u.Exception.ExceptionRecord.ExceptionCode)
+            {
+            case STATUS_BREAKPOINT:
+            {
+
+            }
+            break;
+            }
+        }
+        break;
+        }
+    }
+    break;
+
+    case UE_PLUGIN_CALL_REASON_POSTDEBUG:
+    {
+        //TitanHideCall(ProcessId, UnhidePid);
+    }
+    break;
+    }
+}
+
+extern "C" __declspec(dllexport) bool TitanRegisterPlugin(char* szPluginName, DWORD* titanPluginMajorVersion, DWORD* titanPluginMinorVersion)
+{
+    const DWORD PLUGIN_MAJOR_VERSION = 1;
+    const DWORD PLUGIN_MINOR_VERSION = 0;
+
+    if(titanPluginMajorVersion && titanPluginMinorVersion)
+    {
+        *titanPluginMajorVersion = PLUGIN_MAJOR_VERSION;
+        *titanPluginMinorVersion = PLUGIN_MINOR_VERSION;
+        strcpy(szPluginName, "ScyllaHide");
+        return true;
+    }
+    return false;
+}
