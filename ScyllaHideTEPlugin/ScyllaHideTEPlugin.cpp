e8ca7f3857a4816c77ba8fff334cb5d95a680f09
cypherpunk <the.cypher@gmail.com>
2014-04-07 01:53:37 +0200

    TE plugin version also set to 0.1 as the others

--- ScyllaHideTEPlugin/ScyllaHideTEPlugin.cpp
+++ ScyllaHideTEPlugin/ScyllaHideTEPlugin.cpp
@@ -11,59 +11,59 @@ WCHAR NtApiIniPath[MAX_PATH] = { 0 };
 
 BOOL WINAPI DllMain(HINSTANCE hi, DWORD reason, LPVOID reserved)
 {
-	if (reason == DLL_PROCESS_ATTACH)
-	{
-		GetModuleFileNameW(hi, NtApiIniPath, _countof(NtApiIniPath));
-		WCHAR *temp = wcsrchr(NtApiIniPath, L'\\');
-		if (temp)
-		{
-			temp++;
-			*temp = 0;
-			wcscpy(ScyllaHideDllPath, NtApiIniPath);
-			wcscat(ScyllaHideDllPath, ScyllaHideDllFilename);
-			wcscat(NtApiIniPath, NtApiIniFilename);
-		}
-	}
-	return TRUE;
+    if (reason == DLL_PROCESS_ATTACH)
+    {
+        GetModuleFileNameW(hi, NtApiIniPath, _countof(NtApiIniPath));
+        WCHAR *temp = wcsrchr(NtApiIniPath, L'\\');
+        if (temp)
+        {
+            temp++;
+            *temp = 0;
+            wcscpy(ScyllaHideDllPath, NtApiIniPath);
+            wcscat(ScyllaHideDllPath, ScyllaHideDllFilename);
+            wcscat(NtApiIniPath, NtApiIniFilename);
+        }
+    }
+    return TRUE;
 };
 
 static DWORD SetDebugPrivileges()
 {
-	DWORD err = 0;
-	TOKEN_PRIVILEGES Debug_Privileges;
-	if (!LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &Debug_Privileges.Privileges[0].Luid)) return GetLastError();
+    DWORD err = 0;
+    TOKEN_PRIVILEGES Debug_Privileges;
+    if (!LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &Debug_Privileges.Privileges[0].Luid)) return GetLastError();
 
-	HANDLE hToken = 0;
-	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken))
-	{
-		err = GetLastError();
-		if (hToken) CloseHandle(hToken);
-		return err;
-	}
+    HANDLE hToken = 0;
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken))
+    {
+        err = GetLastError();
+        if (hToken) CloseHandle(hToken);
+        return err;
+    }
 
-	Debug_Privileges.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
-	Debug_Privileges.PrivilegeCount = 1;
+    Debug_Privileges.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+    Debug_Privileges.PrivilegeCount = 1;
 
-	if (!AdjustTokenPrivileges(hToken, false, &Debug_Privileges, 0, NULL, NULL))
-	{
-		err = GetLastError();
-		if (hToken) CloseHandle(hToken);
-	}
+    if (!AdjustTokenPrivileges(hToken, false, &Debug_Privileges, 0, NULL, NULL))
+    {
+        err = GetLastError();
+        if (hToken) CloseHandle(hToken);
+    }
 
-	return err;
+    return err;
 }
 
 static void ScyllaHide(DWORD ProcessId)
 {
-	SetDebugPrivileges(); //set debug privilege
-	ReadNtApiInformation(); //read rva stuff
-	startInjection(ProcessId, ScyllaHideDllPath); //inject
+    SetDebugPrivileges(); //set debug privilege
+    ReadNtApiInformation(); //read rva stuff
+    startInjection(ProcessId, ScyllaHideDllPath); //inject
 }
 
 extern "C" __declspec(dllexport) void TitanDebuggingCallBack(LPDEBUG_EVENT debugEvent, int CallReason)
 {
-	static bool bHooked;
-	static DWORD ProcessId;
+    static bool bHooked;
+    static DWORD ProcessId;
 
     switch(CallReason)
     {
@@ -74,7 +74,7 @@ extern "C" __declspec(dllexport) void TitanDebuggingCallBack(LPDEBUG_EVENT debug
         case CREATE_PROCESS_DEBUG_EVENT:
         {
             ProcessId=debugEvent->dwProcessId;
-			bHooked = false;
+            bHooked = false;
         }
         break;
 
@@ -84,11 +84,11 @@ extern "C" __declspec(dllexport) void TitanDebuggingCallBack(LPDEBUG_EVENT debug
             {
             case STATUS_BREAKPOINT:
             {
-				if (!bHooked)
-				{
-					bHooked = true;
-					ScyllaHide(ProcessId);
-				}
+                if (!bHooked)
+                {
+                    bHooked = true;
+                    ScyllaHide(ProcessId);
+                }
             }
             break;
             }
@@ -102,8 +102,8 @@ extern "C" __declspec(dllexport) void TitanDebuggingCallBack(LPDEBUG_EVENT debug
 
 extern "C" __declspec(dllexport) bool TitanRegisterPlugin(char* szPluginName, DWORD* titanPluginMajorVersion, DWORD* titanPluginMinorVersion)
 {
-    const DWORD PLUGIN_MAJOR_VERSION = 1;
-    const DWORD PLUGIN_MINOR_VERSION = 0;
+    const DWORD PLUGIN_MAJOR_VERSION = 0;
+    const DWORD PLUGIN_MINOR_VERSION = 1;
 
     if(titanPluginMajorVersion && titanPluginMinorVersion)
     {
