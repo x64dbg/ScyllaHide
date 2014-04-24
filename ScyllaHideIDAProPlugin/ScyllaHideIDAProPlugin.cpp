276fe7d7291869f7756935aeac41e53802bba3e6
cypherpunk <the.cypher@gmail.com>
2014-04-24 19:16:42 +0200

    early IDA-Pro plugin. only loads and shows options window

---
+++ ScyllaHideIDAProPlugin/ScyllaHideIDAProPlugin.cpp
@@ -0,0 +1,278 @@
+#include <ida.hpp>
+#define USE_STANDARD_FILE_FUNCTIONS
+#pragma warning(disable : 4996 4512 4127 4201)
+
+#include <Windows.h>
+#include <idp.hpp>
+#include <dbg.hpp>
+#include <loader.hpp>
+#include <kernwin.hpp>
+#include "resource.h"
+
+#define VERSION "0.5"
+
+//globals
+HINSTANCE hinst;
+
+int idaapi debug_mainloop(void *user_data, int notif_code, va_list va);
+
+BOOL WINAPI DllMain(HINSTANCE hi,DWORD reason,LPVOID reserved)
+{
+    if (reason==DLL_PROCESS_ATTACH)
+    {
+        hinst=hi;
+    }
+    return TRUE;
+};
+
+//options dialog proc
+INT_PTR CALLBACK OptionsProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_INITDIALOG:
+    {
+        //LoadOptions();
+
+        /*
+        SendMessage(GetDlgItem(hWnd, IDC_PEBBEINGDEBUGGED), BM_SETCHECK, pHideOptions.PEBBeingDebugged, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_PEBHEAPFLAGS), BM_SETCHECK, pHideOptions.PEBHeapFlags, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_PEBNTGLOBALFLAG), BM_SETCHECK, pHideOptions.PEBNtGlobalFlag, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_PEBSTARTUPINFO), BM_SETCHECK, pHideOptions.PEBStartupInfo, 0);
+        if(pHideOptions.PEBBeingDebugged && pHideOptions.PEBHeapFlags && pHideOptions.PEBNtGlobalFlag && pHideOptions.PEBStartupInfo)
+            SendMessage(GetDlgItem(hWnd, IDC_PEB), BM_SETCHECK, 1, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_NTSETINFORMATIONTHREAD), BM_SETCHECK, pHideOptions.NtSetInformationThread, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_NTQUERYSYSTEMINFORMATION), BM_SETCHECK, pHideOptions.NtQuerySystemInformation, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_NTQUERYINFORMATIONPROCESS), BM_SETCHECK, pHideOptions.NtQueryInformationProcess, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_NTQUERYOBJECT), BM_SETCHECK, pHideOptions.NtQueryObject, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_NTYIELDEXECUTION), BM_SETCHECK, pHideOptions.NtYieldExecution, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_GETTICKCOUNT), BM_SETCHECK, pHideOptions.GetTickCount, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_OUTPUTDEBUGSTRINGA), BM_SETCHECK, pHideOptions.OutputDebugStringA, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_BLOCKINPUT), BM_SETCHECK, pHideOptions.BlockInput, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_NTGETCONTEXTTHREAD), BM_SETCHECK, pHideOptions.NtGetContextThread, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_NTSETCONTEXTTHREAD), BM_SETCHECK, pHideOptions.NtSetContextThread, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_NTCONTINUE), BM_SETCHECK, pHideOptions.NtContinue, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_KIUED), BM_SETCHECK, pHideOptions.KiUserExceptionDispatcher, 0);
+        if(pHideOptions.NtGetContextThread && pHideOptions.NtSetContextThread && pHideOptions.NtContinue && pHideOptions.KiUserExceptionDispatcher)
+            SendMessage(GetDlgItem(hWnd, IDC_PROTECTDRX), BM_SETCHECK, 1, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_NTUSERFINDWINDOWEX), BM_SETCHECK, pHideOptions.NtUserFindWindowEx, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_NTUSERBUILDHWNDLIST), BM_SETCHECK, pHideOptions.NtUserBuildHwndList, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_NTUSERQUERYWINDOW), BM_SETCHECK, pHideOptions.NtUserQueryWindow, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_NTSETDEBUGFILTERSTATE), BM_SETCHECK, pHideOptions.NtSetDebugFilterState, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_NTCLOSE), BM_SETCHECK, pHideOptions.NtClose, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_NTCREATETHREADEX), BM_SETCHECK, pHideOptions.NtCreateThreadEx, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_PREVENTTHREADCREATION), BM_SETCHECK, pHideOptions.preventThreadCreation, 0);
+        SetDlgItemTextW(hWnd, IDC_OLLYTITLE, pHideOptions.ollyTitle);
+        SendMessage(GetDlgItem(hWnd, IDC_DLLSTEALTH), BM_SETCHECK, pHideOptions.DLLStealth, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_DLLNORMAL), BM_SETCHECK, pHideOptions.DLLNormal, 0);
+        SendMessage(GetDlgItem(hWnd, IDC_DLLUNLOAD), BM_SETCHECK, pHideOptions.DLLUnload, 0);*/
+
+        break;
+    }
+    case WM_CLOSE:
+    {
+        EndDialog(hWnd, NULL);
+    }
+    break;
+
+    case WM_COMMAND:
+    {
+        switch(LOWORD(wParam))
+        {
+        case IDOK:
+        {
+            //save options to ini
+            //SaveOptions(hWnd);
+            /*
+            if (ProcessId)
+            {
+                startInjection(ProcessId, ScyllaHideDllPath, true);
+                bHooked = true;
+                MessageBoxA(hWnd, "Applied changes! Restarting target is NOT necessary!", "[ScyllaHide Options]", MB_OK | MB_ICONINFORMATION);
+            }
+            else
+            {
+                MessageBoxA(hWnd, "Please start the target to apply changes!", "[ScyllaHide Options]", MB_OK | MB_ICONINFORMATION);
+            }*/
+
+            EndDialog(hWnd, NULL);
+            break;
+        }
+        case IDC_PROTECTDRX:
+        {
+            WPARAM state;
+            (BST_CHECKED == SendMessage(GetDlgItem(hWnd, IDC_PROTECTDRX), BM_GETCHECK, 0, 0))?state=1:state=0;
+
+            //trigger child checkboxes
+            SendMessage(GetDlgItem(hWnd, IDC_NTGETCONTEXTTHREAD), BM_SETCHECK, state, 0);
+            SendMessage(GetDlgItem(hWnd, IDC_NTSETCONTEXTTHREAD), BM_SETCHECK, state, 0);
+            SendMessage(GetDlgItem(hWnd, IDC_NTCONTINUE), BM_SETCHECK, state, 0);
+            SendMessage(GetDlgItem(hWnd, IDC_KIUED), BM_SETCHECK, state, 0);
+
+            break;
+        }
+        case IDC_NTGETCONTEXTTHREAD:
+        case IDC_NTSETCONTEXTTHREAD:
+        case IDC_NTCONTINUE:
+        case IDC_KIUED:
+        {   //this is just for GUI continuity
+            int allChecked = 1;
+            if(BST_UNCHECKED == SendMessage(GetDlgItem(hWnd, IDC_NTGETCONTEXTTHREAD), BM_GETCHECK, 0, 0)) allChecked--;
+            if(BST_UNCHECKED == SendMessage(GetDlgItem(hWnd, IDC_NTSETCONTEXTTHREAD), BM_GETCHECK, 0, 0)) allChecked--;
+            if(BST_UNCHECKED == SendMessage(GetDlgItem(hWnd, IDC_NTCONTINUE), BM_GETCHECK, 0, 0)) allChecked--;
+            if(BST_UNCHECKED == SendMessage(GetDlgItem(hWnd, IDC_KIUED), BM_GETCHECK, 0, 0)) allChecked--;
+
+            if(allChecked<1) SendMessage(GetDlgItem(hWnd, IDC_PROTECTDRX), BM_SETCHECK, 0, 0);
+            else SendMessage(GetDlgItem(hWnd, IDC_PROTECTDRX), BM_SETCHECK, 1, 0);
+
+            break;
+        }
+        case IDC_PEB:
+        {
+            WPARAM state;
+            (BST_CHECKED == SendMessage(GetDlgItem(hWnd, IDC_PEB), BM_GETCHECK, 0, 0))?state=1:state=0;
+
+            //trigger child checkboxes
+            SendMessage(GetDlgItem(hWnd, IDC_PEBBEINGDEBUGGED), BM_SETCHECK, state, 0);
+            SendMessage(GetDlgItem(hWnd, IDC_PEBHEAPFLAGS), BM_SETCHECK, state, 0);
+            SendMessage(GetDlgItem(hWnd, IDC_PEBNTGLOBALFLAG), BM_SETCHECK, state, 0);
+            SendMessage(GetDlgItem(hWnd, IDC_PEBSTARTUPINFO), BM_SETCHECK, state, 0);
+
+            break;
+        }
+        case IDC_PEBBEINGDEBUGGED:
+        case IDC_PEBHEAPFLAGS:
+        case IDC_PEBNTGLOBALFLAG:
+        case IDC_PEBSTARTUPINFO:
+        {
+            int allChecked = 1;
+            if(BST_UNCHECKED == SendMessage(GetDlgItem(hWnd, IDC_PEBBEINGDEBUGGED), BM_GETCHECK, 0, 0)) allChecked--;
+            if(BST_UNCHECKED == SendMessage(GetDlgItem(hWnd, IDC_PEBHEAPFLAGS), BM_GETCHECK, 0, 0)) allChecked--;
+            if(BST_UNCHECKED == SendMessage(GetDlgItem(hWnd, IDC_PEBNTGLOBALFLAG), BM_GETCHECK, 0, 0)) allChecked--;
+            if(BST_UNCHECKED == SendMessage(GetDlgItem(hWnd, IDC_PEBSTARTUPINFO), BM_GETCHECK, 0, 0)) allChecked--;
+
+            if(allChecked<1) SendMessage(GetDlgItem(hWnd, IDC_PEB), BM_SETCHECK, 0, 0);
+            else SendMessage(GetDlgItem(hWnd, IDC_PEB), BM_SETCHECK, 1, 0);
+            break;
+        }
+        }
+    }
+    break;
+
+    default:
+    {
+        return FALSE;
+    }
+    }
+
+    return 0;
+}
+
+//init the plugin
+int IDAP_init(void)
+{
+    //ensure target is PE executable
+    if (inf.filetype != f_PE) return PLUGIN_SKIP;
+
+    //install hook for debug mainloop
+    if (!hook_to_notification_point(HT_DBG, debug_mainloop, NULL))
+    {
+        msg("[ScyllaHide] Error hooking notification point\n");
+        return PLUGIN_SKIP;
+    }
+
+    msg("#################################################\n");
+    msg("# ScyllaHide "VERSION" Copyright 2014 Aguila / cypher #\n");
+    msg("#################################################\n");
+
+    return PLUGIN_KEEP;
+}
+
+//cleanup on plugin unload
+void IDAP_term(void)
+{
+    unhook_from_notification_point(HT_DBG, debug_mainloop, NULL);
+
+    return;
+}
+
+//called when user clicks in plugin menu or presses hotkey
+void IDAP_run(int arg)
+{
+    DialogBox(hinst, MAKEINTRESOURCE(IDD_OPTIONS), (HWND)callui(ui_get_hwnd).vptr, &OptionsProc);
+
+    return;
+}
+
+int idaapi debug_mainloop(void *user_data, int notif_code, va_list va)
+{
+    switch (notif_code)
+    {
+    case dbg_process_attach:
+    {
+        const debug_event_t* dbgEvent = va_arg(va, const debug_event_t*);
+
+    }
+    break;
+
+    case dbg_process_start:
+    {
+        const debug_event_t* dbgEvent = va_arg(va, const debug_event_t*);
+
+    }
+    break;
+
+    case dbg_process_exit:
+    {
+
+    }
+    break;
+
+    case dbg_library_load:
+    {
+
+    }
+    break;
+
+    case dbg_bpt:
+    {
+        thid_t tid = va_arg(va, thid_t);
+        ea_t breakpoint_ea = va_arg(va, ea_t);
+        va_arg(va, int*);
+    }
+    break;
+
+    case dbg_exception:
+    {
+        const debug_event_t* dbgEvent = va_arg(va, const debug_event_t*);
+
+    }
+    break;
+    }
+
+    return 0;
+}
+
+// There isn't much use for these yet, but I set them anyway.
+char IDAP_comment[] 	= "ScyllaHide usermode Anti-Anti-Debug Plugin";
+char IDAP_help[] 		= "ScyllaHide";
+
+// The name of the plug-in displayed in the Edit->Plugins menu
+char IDAP_name[] 		= "ScyllaHide";
+
+// The hot-key the user can use to run your plug-in.
+char IDAP_hotkey[] 	= "Alt-X";
+
+// The all-important exported PLUGIN object
+plugin_t PLUGIN =
+{
+    IDP_INTERFACE_VERSION,
+    0,
+    IDAP_init,
+    IDAP_term,
+    IDAP_run,
+    IDAP_comment,
+    IDAP_help,
+    IDAP_name,
+    IDAP_hotkey
+};
