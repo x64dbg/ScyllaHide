02159307c4b0b0b2eb8c3ddbaebeaee1fbe548fd
dfgdfgdfg <m8911@gmx.de>
2014-04-27 12:46:32 +0200

    ida server almost working

---
+++ ScyllaHideIDAServer/IdaServerExchange.h
@@ -0,0 +1,126 @@
+#pragma once
+
+enum server_dbg_notification_t
+{
+	dbg_null = 0,
+
+	// debugger low-level event notifications (see IDD.HPP for details).
+
+	dbg_process_start,   // Parameter:  const debug_event_t *event
+	//   This event notification is also an asynchronous
+	//   function result notification for start_process() !
+
+	dbg_process_exit,    // Parameter:  const debug_event_t *event
+	//   This event notification is also an asynchronous
+	//   function result notification for exit_process() !
+
+	dbg_process_attach,  // Parameter:  const debug_event_t *event
+	//   This event notification is also an asynchronous
+	//   function result notification for attach_process() !
+
+	dbg_process_detach,  // Parameter:  const debug_event_t *event
+	//   This event notification is also an asynchronous
+	//   function result notification for detach_process() !
+
+	dbg_thread_start,    // Parameter:  const debug_event_t *event
+
+	dbg_thread_exit,     // Parameter:  const debug_event_t *event
+
+	dbg_library_load,    // Parameter:  const debug_event_t *event
+
+	dbg_library_unload,  // Parameter:  const debug_event_t *event
+
+	dbg_information,     // Parameter:  const debug_event_t *event
+
+	dbg_exception,       // Parameters: const debug_event_t *event
+	//             int                 *warn = -1
+	//             Return (in *warn):
+	//              -1 - to display an exception warning dialog
+	//                   if the process is suspended.
+	//               0 - to never display an exception warning dialog.
+	//               1 - to always display an exception warning dialog.
+
+	// debugger high-level event notifications
+
+	dbg_suspend_process, // The process is now suspended.
+	// Parameter: const debug_event_t *event
+	//   This event notification is also an asynchronous
+	//   function result notification for suspend_process() !
+
+	dbg_bpt,             // A user defined breakpoint was reached.
+	// Parameters: thid_t tid
+	//             ea_t        bptea
+	//             int        *warn = -1
+	//             Return (in *warn):
+	//              -1 - to display a breakpoint warning dialog
+	//                   if the process is suspended.
+	//               0 - to never display a breakpoint warning dialog.
+	//               1 - to always display a breakpoint warning dialog.
+
+	dbg_trace,           // A step occured (one instruction was executed). This event
+	// notification is only generated if step tracing is enabled.
+	// Parameters: thid_t tid
+	//             ea_t        ip
+	// Returns: 1-do not log this trace event; 0-log it
+
+	dbg_request_error,   // An error occured during the processing of a request.
+	// Parameters: ui_notification_t  failed_command
+	//             dbg_notification_t failed_dbg_notification
+
+	dbg_step_into,       // Parameter: const debug_event_t *event
+
+	dbg_step_over,       // Parameter: const debug_event_t *event
+
+	dbg_run_to,          // Parameter: const debug_event_t *event
+
+	dbg_step_until_ret,  // Parameter: const debug_event_t *event
+
+	dbg_bpt_changed,     // Breakpoint has been changed
+
+	inject_dll
+};
+
+#define RESULT_SUCCESS 1
+#define RESULT_FAILED 0
+
+typedef struct _IDA_SERVER_EXCHANGE
+{
+	unsigned long result;
+	unsigned long notif_code; //server_dbg_notification_t
+	unsigned long ProcessId;
+
+	unsigned char EnablePebBeingDebugged;
+	unsigned char EnablePebHeapFlags;
+	unsigned char EnablePebNtGlobalFlag;
+	unsigned char EnablePebStartupInfo;
+
+	unsigned char EnableBlockInputHook;
+	unsigned char EnableGetTickCountHook;
+	unsigned char EnableOutputDebugStringHook;
+
+	unsigned char EnableNtSetInformationThreadHook;
+	unsigned char EnableNtQuerySystemInformationHook;
+	unsigned char EnableNtQueryInformationProcessHook;
+	unsigned char EnableNtQueryObjectHook;
+	unsigned char EnableNtYieldExecutionHook;
+	unsigned char EnableNtCloseHook;
+
+	unsigned char EnablePreventThreadCreation;
+	unsigned char EnableNtCreateThreadExHook;
+
+	//Protect and Hide Hardware Breakpoints
+	unsigned char EnableNtGetContextThreadHook;
+	unsigned char EnableNtSetContextThreadHook;
+	unsigned char EnableNtContinueHook;
+	unsigned char EnableKiUserExceptionDispatcherHook;
+
+	unsigned char EnableNtUserQueryWindowHook;
+	unsigned char EnableNtUserBuildHwndListHook;
+	unsigned char EnableNtUserFindWindowExHook;
+	unsigned char EnableNtSetDebugFilterStateHook;
+
+	unsigned char DllInjectStealth;
+	unsigned char DllInjectNormal;
+	unsigned char UnloadDllAfterInjection;
+	wchar_t DllPathForInjection[300];
+} IDA_SERVER_EXCHANGE;
--- ScyllaHideIDAServer/idaserver.cpp
+++ ScyllaHideIDAServer/idaserver.cpp
@@ -1,8 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "idaserver.h"
+#include "IdaServerExchange.h"
 
+#include "..\ScyllaHideOlly2Plugin\ScyllaHideVersion.h"
+
+#include "..\ScyllaHideOlly2Plugin\Injector.h"
+#include "..\InjectorCLI\ReadNtConfig.h"
 
 WSADATA wsaData;
-BOOL isWinsockUp;
 
 
 char * ListenPortString = IDA_SERVER_DEFAULT_PORT_TEXT;
@@ -10,24 +15,62 @@ unsigned short ListenPort = IDA_SERVER_DEFAULT_PORT;
 
 IDA_SERVER_EXCHANGE idaExchange = {0};
 
+struct HideOptions pHideOptions = {0};
+
+#ifdef _WIN64
+const WCHAR ScyllaHideDllFilename[] = L"HookLibraryx64.dll";
+#else
+const WCHAR ScyllaHideDllFilename[] = L"HookLibraryx86.dll";
+#endif
+
+const WCHAR NtApiIniFilename[] = L"NtApiCollection.ini";
+
+WCHAR ScyllaHideDllPath[MAX_PATH] = {0};
+WCHAR NtApiIniPath[MAX_PATH] = {0};
+
+DWORD SetDebugPrivileges();
+BOOL FileExists(LPCWSTR szPath);
+void checkPaths();
+
+
+extern HOOK_DLL_EXCHANGE DllExchangeLoader;
+
+typedef void (__cdecl * t_LogWrapper)(const WCHAR * format, ...);
+
+void LogWrapper(const WCHAR * format, ...);
+extern t_LogWrapper LogWrap;
+extern t_LogWrapper LogErrorWrap;
+
 int main(int argc, char *argv[])
 {
-	printf("size of IDA_SERVER_EXCHANGE %d\n\n", sizeof(IDA_SERVER_EXCHANGE));
+	LogWrap = LogWrapper;
+	LogErrorWrap = LogWrapper;
+
+	if (sizeof(IDA_SERVER_EXCHANGE) != 638)
+	{
+		printf("WRONG!!! Size of IDA_SERVER_EXCHANGE %d == 638?\n\n", sizeof(IDA_SERVER_EXCHANGE));
+		return 0;
+	}
 
 
-	printf("Starting IDA Server...\n");
+	SetDebugPrivileges();
+
+	printf("Starting IDA Server v" SCYLLA_HIDE_VERSION_STRING_A "...\n");
+
+	checkPaths();
+
 
 	if (argc > 1)
 	{
 		ListenPortString = argv[1];
 		ListenPort = (unsigned short)strtoul(ListenPortString, 0, 10);
 	}
-	printf("Listen Port: %d\n", ListenPort);
+	printf("Listen Port: %d (0x%X)\n", ListenPort, ListenPort);
 
 
 	if (startWinsock())
 	{
-		printf("Starting Winsock: DONE\n");
+		//printf("Starting Winsock: DONE\n");
 		startListen();
 	}
 	
@@ -36,7 +79,37 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
+BOOL FileExists(LPCWSTR szPath)
+{
+	DWORD dwAttrib = GetFileAttributes(szPath);
 
+	return (dwAttrib != INVALID_FILE_ATTRIBUTES && 
+		!(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+}
+
+void checkPaths()
+{
+	GetModuleFileNameW(0, NtApiIniPath, _countof(NtApiIniPath));
+	WCHAR *temp = wcsrchr(NtApiIniPath, L'\\');
+	if (temp)
+	{
+		temp++;
+		*temp = 0;
+		wcscpy(ScyllaHideDllPath, NtApiIniPath);
+		wcscat(ScyllaHideDllPath, ScyllaHideDllFilename);
+		wcscat(NtApiIniPath, NtApiIniFilename);
+	}
+
+	if (!FileExists(ScyllaHideDllPath))
+	{
+		wprintf(L"File missing %s\n", ScyllaHideDllPath);
+	}
+	if (!FileExists(NtApiIniPath))
+	{
+		wprintf(L"File missing %s\n", NtApiIniPath);
+	}
+
+}
 
 void startListen()
 {
@@ -47,8 +120,6 @@ void startListen()
 
 	struct addrinfo *result = NULL;
 	struct addrinfo hints;
-
-	int iSendResult;
 
 	ZeroMemory(&hints, sizeof(hints));
 	hints.ai_family = AF_INET;
@@ -99,29 +170,64 @@ void startListen()
 
 	printf("Listening on port %s...\n",ListenPortString);
 
+	int count = 0;
+
 	while(1)
 	{
 		ClientSocket = accept(ListenSocket, NULL, NULL);
 		if (ClientSocket == INVALID_SOCKET)
 		{
 			printf("accept failed with error: %d\n", WSAGetLastError());
-			closesocket(ListenSocket);
-			WSACleanup();
-			return;
+			break;
 		}
 		else
 		{
-			printf("Accepted a client\n");
+			count++;
+
+			printf("Accepted Client %d\n", count);
 			handleClient(ClientSocket);
 			closesocket(ClientSocket);
 		}
 	}
 
+	closesocket(ListenSocket);
 
+	WSACleanup();
 }
 
 static DWORD ProcessId = 0;
 static bool bHooked = false;
+
+void MapSettings()
+{
+	pHideOptions.DLLUnload = idaExchange.UnloadDllAfterInjection;
+	pHideOptions.DLLNormal = idaExchange.DllInjectNormal;
+	pHideOptions.DLLStealth = idaExchange.DllInjectStealth;
+	pHideOptions.KiUserExceptionDispatcher = idaExchange.EnableKiUserExceptionDispatcherHook;
+	pHideOptions.NtClose = idaExchange.EnableNtCloseHook;
+	pHideOptions.NtContinue = idaExchange.EnableNtCloseHook;
+	pHideOptions.NtCreateThreadEx = idaExchange.EnableNtCreateThreadExHook;
+	pHideOptions.NtGetContextThread = idaExchange.EnableNtGetContextThreadHook;
+	pHideOptions.NtQueryInformationProcess = idaExchange.EnableNtQueryInformationProcessHook;
+	pHideOptions.NtQueryObject = idaExchange.EnableNtQueryObjectHook;
+	pHideOptions.NtQuerySystemInformation = idaExchange.EnableNtQuerySystemInformationHook;
+	pHideOptions.NtSetContextThread = idaExchange.EnableNtSetContextThreadHook;
+	pHideOptions.NtSetDebugFilterState = idaExchange.EnableNtSetDebugFilterStateHook;
+	pHideOptions.NtSetInformationThread = idaExchange.EnableNtSetInformationThreadHook;
+	pHideOptions.NtUserBuildHwndList = idaExchange.EnableNtUserBuildHwndListHook;
+	pHideOptions.NtUserFindWindowEx = idaExchange.EnableNtUserFindWindowExHook;
+	pHideOptions.NtUserQueryWindow = idaExchange.EnableNtUserQueryWindowHook;
+	pHideOptions.NtYieldExecution = idaExchange.EnableNtYieldExecutionHook;
+	pHideOptions.preventThreadCreation = idaExchange.EnablePreventThreadCreation;
+	pHideOptions.OutputDebugStringA = idaExchange.EnableOutputDebugStringHook;
+	pHideOptions.GetTickCount = idaExchange.EnableGetTickCountHook;
+	pHideOptions.BlockInput = idaExchange.EnableBlockInputHook;
+
+	pHideOptions.PEBBeingDebugged = idaExchange.EnablePebBeingDebugged;
+	pHideOptions.PEBHeapFlags = idaExchange.EnablePebHeapFlags;
+	pHideOptions.PEBNtGlobalFlag = idaExchange.EnablePebNtGlobalFlag;
+	pHideOptions.PEBStartupInfo = idaExchange.EnablePebStartupInfo;
+}
 
 void handleClient( SOCKET ClientSocket )
 {
@@ -129,8 +235,11 @@ void handleClient( SOCKET ClientSocket )
 	do
 	{
 		iResult = recv(ClientSocket, (char*)&idaExchange, sizeof(IDA_SERVER_EXCHANGE), 0);
+
 		if (iResult == sizeof(IDA_SERVER_EXCHANGE))
 		{
+			MapSettings();
+
 			switch (idaExchange.notif_code)
 			{
 			case dbg_process_attach:
@@ -141,15 +250,15 @@ void handleClient( SOCKET ClientSocket )
 			case dbg_process_start:
 				{
 
-					//ProcessId = idaExchange.ProcessId;
-					//bHooked = false;
-					//ZeroMemory(&DllExchangeLoader, sizeof(HOOK_DLL_EXCHANGE));
+					ProcessId = idaExchange.ProcessId;
+					bHooked = false;
+					ZeroMemory(&DllExchangeLoader, sizeof(HOOK_DLL_EXCHANGE));
 
-					//if (!bHooked)
-					//{
-					//	bHooked = true;
-					//	startInjection(ProcessId, ScyllaHideDllPath, true);
-					//}
+					if (!bHooked)
+					{
+						bHooked = true;
+						startInjection(ProcessId, ScyllaHideDllPath, true);
+					}
 
 					break;
 				}
@@ -161,8 +270,18 @@ void handleClient( SOCKET ClientSocket )
 			case dbg_library_load:
 				{
 
+					if (bHooked)
+					{
+						startInjection(ProcessId, ScyllaHideDllPath, false);
+					}
 					break;
 				}
+
+				case inject_dll:
+					{
+						injectDll(ProcessId, idaExchange.DllPathForInjection);
+						break;
+					}
 			}
 		}
 		else if (iResult == 0)
@@ -183,7 +302,7 @@ void handleClient( SOCKET ClientSocket )
 
 BOOL startWinsock()
 {
-	isWinsockUp = TRUE;
+	BOOL isWinsockUp = TRUE;
 
 	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (iResult != 0)
@@ -195,8 +314,39 @@ BOOL startWinsock()
 	return isWinsockUp;
 }
 
-void closeWinsock()
+DWORD SetDebugPrivileges()
 {
-	WSACleanup();
-	isWinsockUp = FALSE;
+	DWORD err = 0;
+	TOKEN_PRIVILEGES Debug_Privileges;
+	if (!LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &Debug_Privileges.Privileges[0].Luid)) return GetLastError();
+
+	HANDLE hToken = 0;
+	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken))
+	{
+		err = GetLastError();
+		if (hToken) CloseHandle(hToken);
+		return err;
+	}
+
+	Debug_Privileges.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+	Debug_Privileges.PrivilegeCount = 1;
+
+	if (!AdjustTokenPrivileges(hToken, false, &Debug_Privileges, 0, NULL, NULL))
+	{
+		err = GetLastError();
+		if (hToken) CloseHandle(hToken);
+	}
+
+	return err;
+}
+
+void LogWrapper(const WCHAR * format, ...)
+{
+	WCHAR text[2000];
+	va_list va_alist;
+	va_start(va_alist, format);
+
+	wvsprintfW(text, format, va_alist);
+
+	wprintf(text);
 }
