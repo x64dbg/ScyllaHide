7ed863189a49e13c1bb956d62ae96c8987a752c6
deepzero <deep@zero.com>
2014-05-03 23:13:18 +0200

    fix internet handle leaks, fix oob buffer read

--- UpdateCheck/UpdateCheck.cpp
+++ UpdateCheck/UpdateCheck.cpp
@@ -6,53 +6,62 @@
 bool isNewVersionAvailable()
 {
     char buf[1024] = {0};
-    DWORD dwBytesRead;
+    DWORD dwBytesRead=0;
+	HINTERNET hi=0, hCon=0;
+	bool ret=0;
 
-	HINTERNET hi = InternetOpenW(L"ScyllaHide Update Checker", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
-    if(!hi)
+	hi = InternetOpenW(L"ScyllaHide Update Checker", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
+    if(hi)
 	{
-		MessageBoxA(0, "InternetOpenW failed", "ERROR", MB_ICONERROR);
-		return false;
-	}
-
-    WCHAR szHead[] = L"Accept: */*\r\n\r\n";
-	HINTERNET hCon = InternetOpenUrlW(hi, UPDATE_CHECK_URL, szHead, (DWORD)wcslen(szHead), INTERNET_FLAG_PRAGMA_NOCACHE, 0);
-	if(!hCon)
-	{
-		MessageBoxA(0, "InternetOpenUrlW failed", "ERROR", MB_ICONERROR);
-		return false;
-	}
-
-    if(!InternetReadFile(hCon, buf, sizeof(buf), &dwBytesRead))
-	{
-		MessageBoxA(0, "InternetReadFile failed", "ERROR", MB_ICONERROR);
-		return false;
-	}
-
-
-    //compare the versions e.g. 0.7 1.0 0.5.b (lexicographical cmp might not work)
-
-	char * temp = strchr(buf, '.');
-	if (temp)
-	{
-		*temp = 0;
-		temp++;
-
-		int major = strtoul(buf, 0, 10);
-		int minor = strtoul(temp, 0, 10);
-
-		if (major > SCYLLA_HIDE_MAJOR_VERSION)
+		WCHAR szHead[] = L"Accept: */*\r\n\r\n";
+		hCon = InternetOpenUrlW(hi, UPDATE_CHECK_URL, szHead, (DWORD)wcslen(szHead), INTERNET_FLAG_PRAGMA_NOCACHE, 0);
+		if(hCon)
 		{
-			return true;
+			if(InternetReadFile(hCon, buf, sizeof(buf)-5, &dwBytesRead))
+			{
+				//compare the versions e.g. 0.7 1.0 0.5.b (lexicographical cmp might not work)
+
+				char * temp = strchr(buf, '.');
+				if (temp)
+				{
+					*temp = 0;
+					temp++;
+
+					int major = strtoul(buf, 0, 10);
+					int minor = strtoul(temp, 0, 10);
+
+					if (major > SCYLLA_HIDE_MAJOR_VERSION)
+					{
+						ret = true;
+					}
+					else
+					{
+						if (minor > SCYLLA_HIDE_MINOR_VERSION)
+						{
+							ret = true;
+						}
+					}
+				}
+			}
+			else
+			{
+				MessageBoxA(0, "InternetReadFile failed", "ERROR", MB_ICONERROR);
+			}
 		}
 		else
 		{
-			if (minor > SCYLLA_HIDE_MINOR_VERSION)
-			{
-				return true;
-			}
+			MessageBoxA(0, "InternetOpenUrlW failed", "ERROR", MB_ICONERROR);
 		}
 	}
+	else
+	{
+		MessageBoxA(0, "InternetOpenW failed", "ERROR", MB_ICONERROR);
+	}
 
-    return false;
+	if(hi)
+		InternetCloseHandle(hi);
+	if(hCon)
+		InternetCloseHandle(hCon);
+
+	return ret;
 }
