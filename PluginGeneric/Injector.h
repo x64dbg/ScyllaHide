6f362f873636eb3960f0f242ae36fe94e2604dab
dfgdfgdfg <m8911@gmx.de>
2014-05-04 12:34:40 +0200

    special folder for shared files

---
+++ PluginGeneric/Injector.h
@@ -0,0 +1,58 @@
+#include <windows.h>
+#include "..\HookLibrary\HookMain.h"
+#include "..\InjectorCLI\DynamicMapping.h"
+
+struct HideOptions
+{
+    int PEBBeingDebugged;
+    int PEBHeapFlags;
+    int PEBNtGlobalFlag;
+    int PEBStartupInfo;
+    int NtSetInformationThread;
+    int NtQuerySystemInformation;
+    int NtQueryInformationProcess;
+    int NtQueryObject;
+    int NtYieldExecution;
+    int GetTickCount;
+	int GetTickCount64;
+	int GetLocalTime;
+	int GetSystemTime;
+	int NtQuerySystemTime;
+	int NtQueryPerformanceCounter;
+    int OutputDebugStringA;
+    int BlockInput;
+    int NtGetContextThread;
+    int NtSetContextThread;
+    int NtContinue;
+    int KiUserExceptionDispatcher;
+    int NtUserFindWindowEx;
+    int NtUserBuildHwndList;
+    int NtUserQueryWindow;
+    int NtSetDebugFilterState;
+    int NtClose;
+    int NtCreateThreadEx;
+    int removeEPBreak;
+    int fixOllyBugs;
+    int breakTLS;
+    int skipEPOutsideCode;
+    WCHAR ollyTitle[300];
+    int x64Fix;
+    int preventThreadCreation;
+    int DLLStealth;
+    int DLLNormal;
+    int DLLUnload;
+    WCHAR serverPort[6];
+    int autostartServer;
+	int removeDebugPrivileges;
+};
+
+void startInjectionProcess(HANDLE hProcess, BYTE * dllMemory, bool newProcess);
+void startInjection(DWORD targetPid, const WCHAR * dllPath, bool newProcess);
+void injectDll(DWORD targetPid, const WCHAR * dllPath);
+BYTE * ReadFileToMemory(const WCHAR * targetFilePath);
+void FillExchangeStruct(HANDLE hProcess, HOOK_DLL_EXCHANGE * data);
+void StartFixBeingDebugged(DWORD targetPid, bool setToNull);
+
+bool IsProcessWOW64(HANDLE hProcess);
+bool isWindows64();
+bool RemoveDebugPrivileges(HANDLE hProcess);
