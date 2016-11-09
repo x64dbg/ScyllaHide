e6ffb5072c033ab776d6ae44a7b05929f05cfce9
Aleksey Katargin <gureedo@gmail.com>
2016-11-09 22:04:15 +0500

    Reworked HookLibrary/Hook.c (#13)
    Cleaned code.
    Pass correct arch to LDE (credits to Alex Schwartzman)

--- HookLibrary/Hook.h
+++ HookLibrary/Hook.h
@@ -1,14 +1,7 @@
 #pragma once
 
-#ifndef _WIN64
-#pragma comment(lib, "LDE64")
-extern "C" unsigned int __stdcall LDE(const void * lpData, unsigned int size);
-#else
-#pragma comment(lib, "LDE64x64")
-extern "C" unsigned int __fastcall LDE(const void * lpData, unsigned int size);
-#endif
+#include <windows.h>
 
-int GetDetourLen(const void * lpStart, const int minSize);
-void WriteJumper(unsigned char * lpbFrom, unsigned char * lpbTo);
-void * DetourCreate(void * lpFuncOrig, void * lpFuncDetour, bool createTramp);
-
+unsigned GetDetourLen(LPCBYTE lpbStart, unsigned uMinSize);
+void WriteJumper(LPBYTE lpbFrom, LPCBYTE lpcbTo);
+LPBYTE DetourCreate(LPBYTE lpbFuncOrig, LPCBYTE lpcbFuncDetour, BOOL fCreateTramp);
