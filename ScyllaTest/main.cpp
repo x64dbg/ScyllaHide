f64e9c0f24157eec2553b647cda2f6a1963a1932
dummys <dummys1337@gmail.com>
2017-10-17 22:41:32 +0200

    Added check for NtQueryInformationProcess ProcessDebugPort
    fixed Mattiwatti issue

--- ScyllaTest/main.cpp
+++ ScyllaTest/main.cpp
@@ -192,6 +192,13 @@ static ScyllaTestResult Check_OutputDebugStringW_Exception()
     }
 }
 
+static ScyllaTestResult Check_NtQueryInformationProcess_ProcessDebugPort()
+{
+    HANDLE handle = nullptr;
+    SCYLLA_TEST_FAIL_IF(!NT_SUCCESS(NtQueryInformationProcess(g_proc_handle, ProcessDebugPort, &handle, sizeof(handle), nullptr)));
+    return SCYLLA_TEST_CHECK(handle == nullptr);
+}
+
 static const char *ScyllaTestResultAsStr(ScyllaTestResult result)
 {
     switch (result)
@@ -262,6 +269,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     SCYLLA_TEST(OutputDebugStringA_LastError, ver < scl::OS_WIN_VISTA);
     SCYLLA_TEST(OutputDebugStringA_Exception, true);
     SCYLLA_TEST(OutputDebugStringW_Exception, ver >= scl::OS_WIN_10);
+    SCYLLA_TEST(NtQueryInformationProcess_ProcessDebugPort, ver >= scl::OS_WIN_XP);
 
     CloseHandle(g_proc_handle);
     g_proc_handle = INVALID_HANDLE_VALUE;
