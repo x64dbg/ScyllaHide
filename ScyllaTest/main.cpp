c68e3a69a649003634ffed55257c1d7f82894ba1
dummys <dummys1337@gmail.com>
2017-10-19 17:32:03 +0200

    Added check for NtQuerySystemInformation_SystemKernelDebuggerInformation

--- ScyllaTest/main.cpp
+++ ScyllaTest/main.cpp
@@ -199,6 +199,19 @@ static ScyllaTestResult Check_NtQueryInformationProcess_ProcessDebugPort()
     return SCYLLA_TEST_CHECK(handle == nullptr);
 }
 
+static ScyllaTestResult Check_NtQuerySystemInformation_SystemKernelDebuggerInformation()
+{
+    SYSTEM_KERNEL_DEBUGGER_INFORMATION SysKernDebInfo;
+    HMODULE hNtdll = 0;
+
+    SCYLLA_TEST_FAIL_IF(!NT_SUCCESS(NtQuerySystemInformation(SystemKernelDebuggerInformation, &SysKernDebInfo, sizeof(SysKernDebInfo), NULL)));
+    if (SysKernDebInfo.KernelDebuggerEnabled || !SysKernDebInfo.KernelDebuggerNotPresent)
+    {
+        return ScyllaTestDetected;
+    }
+    return ScyllaTestOk;
+}
+
 static const char *ScyllaTestResultAsStr(ScyllaTestResult result)
 {
     switch (result)
@@ -270,6 +283,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     SCYLLA_TEST(OutputDebugStringA_Exception, true);
     SCYLLA_TEST(OutputDebugStringW_Exception, ver >= scl::OS_WIN_10);
     SCYLLA_TEST(NtQueryInformationProcess_ProcessDebugPort, ver >= scl::OS_WIN_XP);
+    SCYLLA_TEST(NtQuerySystemInformation_SystemKernelDebuggerInformation, ver >= scl::OS_WIN_XP);
 
     CloseHandle(g_proc_handle);
     g_proc_handle = INVALID_HANDLE_VALUE;
