36a85a9b400949f29af9e8896163c27d527380f4
Mattiwatti <mattiwatti@gmail.com>
2017-11-25 23:34:14 +0100

    Remove Hook.cpp/.h as they aren't actually used by anything (this functionality is implemented in RemoteHook.cpp using distorm), and remove LDE64 from the libs dir as it was only required by Hook.cpp in the first place

--- HookLibrary/Hook.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <windows.h>
-
-unsigned GetDetourLen(LPCBYTE lpbStart, unsigned uMinSize);
-void WriteJumper(LPBYTE lpbFrom, LPCBYTE lpcbTo);
-LPBYTE DetourCreate(LPBYTE lpbFuncOrig, LPCBYTE lpcbFuncDetour, BOOL fCreateTramp);
