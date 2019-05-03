78e64c6dabbef239f89c96c506839ded39e09bb4
Mattiwatti <mattiwatti@gmail.com>
2019-05-03 10:27:13 +0200

    Add fallback path to make_scmrev.h.bat in case git is not installed

--- SCMRevGen/make_scmrev.h.bat
+++ SCMRevGen/make_scmrev.h.bat
@@ -1,13 +1,19 @@
 @echo off
 
-set GIT_VERSION_NUMBER=
+set GIT_VERSION_NUMBER=0 
+set GIT_SHORT_HASH=unknown
+
+where git 1>nul 2>&1
+if errorlevel 1 goto :nogit
+
 for /f "usebackq delims=" %%i in (`git rev-list --count HEAD`) do (
 	set GIT_VERSION_NUMBER=%%i
 )
-set GIT_SHORT_HASH=
 for /f "usebackq delims=" %%i in (`git rev-parse --short HEAD`) do (
 	set GIT_SHORT_HASH=%%i
 )
+
+:nogit
 set COMPILE_DATE=
 for /f "usebackq delims=" %%i in (`date.exe +^"%%F %%H:%%M^"`) do (
 	set COMPILE_DATE=%%i
@@ -17,8 +23,8 @@ for /f "usebackq delims=" %%i in (`date.exe +^"%%Y^"`) do (
 	set COMPILE_YEAR=%%i
 )
 
-echo #pragma once > scmrev.h
-echo. >> scmrev.h
+echo #pragma once> scmrev.h
+echo.>> scmrev.h
 echo #define GIT_VERSION_NUMBER %GIT_VERSION_NUMBER%>> scmrev.h
 echo #define GIT_SHORT_HASH_A "%GIT_SHORT_HASH%">> scmrev.h
 echo #define GIT_SHORT_HASH_W L"%GIT_SHORT_HASH%">> scmrev.h
