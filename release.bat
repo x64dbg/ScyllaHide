761ba53b0c2350ace1cf84ed1f53bfb0afd90fb4
Mattiwatti <mattiwatti@gmail.com>
2019-05-23 02:47:27 +0200

    Update release.bat

--- release.bat
+++ release.bat
@@ -1,16 +1,33 @@
 @echo off
+setlocal enabledelayedexpansion
+
+cd %~dp0
 
 @rem build release
-call "%VS120COMNTOOLS%vsvars32.bat"
-msbuild /m /property:Configuration=Release,Platform=Win32
-msbuild /m /property:Configuration=Release,Platform=x64
+set VSWHERE=%PROGRAMFILES(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
+if not exist "%VSWHERE%" (
+	echo VS2017/VS2019 installation directory does not exist, or the vswhere.exe tool is missing.
+	exit
+)
+
+set MSBUILD=
+for /f "usebackq tokens=*" %%i in (`"%VSWHERE%" -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
+	set MSBUILD=%%i
+)
+
+if "%MSBUILD%"=="" (
+	echo Failed to find MSBuild installation directory.
+	exit
+)
+
+"%MSBUILD%" /m /property:Configuration=Release,Platform=Win32
+if not %ERRORLEVEL%==0 exit
+
+"%MSBUILD%" /m /property:Configuration=Release,Platform=x64
+if not %ERRORLEVEL%==0 exit
 
 rmdir /S /Q Release
 xcopy /S /Y ConfigCollection Release\
-
-@rem PDBReader tool
-copy /y /b build\Release\Win32\PDBReaderx86.exe Release\NtApiTool\x86
-copy /y /b build\Release\x64\PDBReaderx64.exe Release\NtApiTool\x64
 
 @rem plugins
 mkdir Release\plugins
@@ -29,4 +46,3 @@ xcopy /S /Y build\Release\Win32\*.exe Release
 xcopy /S /Y build\Release\x64\*.exe Release
 copy /y /b build\Release\Win32\HookLibraryx86.dll Release
 copy /y /b build\Release\x64\HookLibraryx64.dll Release
-del /F /Q Release\PdbReader*.exe
