x64dbg/ScyllaHide

commit 9ad0839dc2302f9c8b7ca7e4494b0ca9195a598d

Fix according to newest x64dbg changes (#114)
* Fix according to newest x64dbg changes

In newest x64dbg version the auto break on attach was removed so ScyllaHide would never inject.

ScyllaHideX64DBGPlugin/ScyllaHideX64DBGPlugin.cpp
@@ -164,6 +164,14 @@ static void cbDebugloop(CBTYPE cbType, void* callbackInfo)
                     MessageBoxW(hwndDlg, L"Anti-Anti-Attach failed", L"Error", MB_ICONERROR);
                 }
             }
+            //In newest x64dbg version the auto break on attach was removed so ScyllaHide would never inject.
+            if (!bHooked)
+            {
+                ReadNtApiInformation(&g_hdd);
+
+                bHooked = true;
+                startInjection(ProcessId, &g_hdd, g_scyllaHideDllPath.c_str(), true);
+            }
         }
 
         break;
@@ -182,14 +190,6 @@ static void cbDebugloop(CBTYPE cbType, void* callbackInfo)
         {
         case STATUS_BREAKPOINT:
         {
-            if (!bHooked)
-            {
-                ReadNtApiInformation(&g_hdd);
-
-                bHooked = true;
-                startInjection(ProcessId, &g_hdd, g_scyllaHideDllPath.c_str(), true);
-            }
-
             break;
         }
 

commit 37ff0b36597664873efad5cbc088d3ac0dcf5285

Fix regression in x64dbg plugin caused by 9ad0839d. Fixes #115

ScyllaHideX64DBGPlugin/ScyllaHideX64DBGPlugin.cpp
@@ -190,6 +190,13 @@ static void cbDebugloop(CBTYPE cbType, void* callbackInfo)
         {
         case STATUS_BREAKPOINT:
         {
+            if (!bHooked)
+            {
+                ReadNtApiInformation(&g_hdd);
+
+                bHooked = true;
+                startInjection(ProcessId, &g_hdd, g_scyllaHideDllPath.c_str(), true);
+            }
             break;
         }
 